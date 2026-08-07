49d540942d2a23b8380165b09f580a61e227bda0
Niarosss <suharevich.vadim@gmail.com>
2026-08-07 16:05:42 +0300

    feat: add vite-plugin-css-injected-by-js for improved CSS handling; remove unused SVG files

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    cssInjectedByJs(),
 
     babel({ presets: [reactCompilerPreset()] })
   ],
